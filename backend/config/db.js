62fae9c379297088fbcce635bf48828f864c6887
sushma <sushmap9544@gmail.com>
2026-03-04 12:48:04 +0530

    commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,11 @@
 const { Pool } = require("pg");
-require("dotenv").config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  user: "postgres",
+  host: "localhost",
+  database: "vindia_erp",
+  password: "postgres123",
+  port: 5432,
 });
 
 module.exports = pool;
