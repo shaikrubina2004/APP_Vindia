1a9b57e11655c265dad571e4b7cb81ac88869196
ABHINANDHANA <abhinandhanam2002@gmail.com>
2026-03-31 15:25:47 +0530

    EMP

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const attendanceRoutes = require("./routes/attendanceRoutes");
 const leaveRoutes = require("./routes/leaveRoutes");
 //const payrollRoutes = require("./routes/payrollRoutes");
 const employeeRoutes = require("./routes/employeeRoutes");
-
+const path = require("path");
 /* ✅ ADDED PROJECT ROUTES */
 const projectRoutes = require("./routes/projectRoutes");
 
@@ -47,7 +47,7 @@ try {
   app.use("/api/employees", employeeRoutes);
   app.use("/api/attendance", attendanceRoutes);
   app.use("/api/leaves", leaveRoutes);
-  
+  app.use("/uploads", express.static(path.join(__dirname, "uploads")));
   //app.use("/api/payroll", payrollRoutes);
 
   /* ✅ ADDED PROJECT ROUTE */
