2747094ce33ff8531f139d62fcf22fa12e9a6cb5
shaikrubina2004 <shaikrubina2004@gmail.com>
2026-04-30 11:11:50 +0530

    Update StructuralEngineerLayout.jsx

--- app_vindia/src/layouts/StructuralEngineerLayout.jsx
+++ app_vindia/src/layouts/StructuralEngineerLayout.jsx
@@ -1,20 +1,22 @@
-// FILE PATH: src/layouts/StructuralEngineerLayout.jsx
-
-import { Outlet }                  from "react-router-dom";
-import AppLayout                   from "./AppLayout";
-import { StructuralEngineerMenu }  from "../menus/structuralEngineerMenu";
-import { SENotificationProvider }  from "../context/SENotificationProvider";
-import SENotificationBell          from "../components/notifications/SENotificationBell";
+import { Outlet } from "react-router-dom";
+import AppLayout from "./AppLayout";
+import { StructuralEngineerMenu } from "../menus/structuralEngineerMenu";
+import { SENotificationProvider } from "../context/SENotificationProvider";
+import SENotificationBell from "../components/notifications/SENotificationBell";
+import { ProjectProvider } from "../context/ProjectContext";
 
 export default function StructuralEngineerLayout() {
   return (
     <SENotificationProvider>
-      <AppLayout
-        menuItems={StructuralEngineerMenu}
-        notificationSlot={<SENotificationBell />}
-      >
-        <Outlet />
-      </AppLayout>
+      <ProjectProvider>
+        {" "}
+        <AppLayout
+          menuItems={StructuralEngineerMenu}
+          notificationSlot={<SENotificationBell />}
+        >
+          <Outlet />
+        </AppLayout>
+      </ProjectProvider>
     </SENotificationProvider>
   );
 }
